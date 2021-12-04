95ec517dbd0e354d6fcb10064efd55a2c0d04d28
Ardent Eliot Reinhard <ardent@ucsc.edu>
2021-12-04 01:04:33 -0800

    adding the base of the ink ported into unity

---
+++ Assets/scripts/inkTestingScript.cs
@@ -0,0 +1,81 @@
+using Ink.Runtime;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class inkTestingScript : MonoBehaviour
+{
+    public TextAsset inkJSON;
+    private Story story;
+
+    public Text textPrefab;
+    public Button buttonPrefab;
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        story = new Story(inkJSON.text);
+
+        refreshUI();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void refreshUI()
+    {
+
+        eraseUI();
+
+        Text storyText = Instantiate(textPrefab) as Text;
+        storyText.text = loadStoryChunk();
+        storyText.transform.SetParent(this.transform, false);
+
+        foreach (Choice choice in story.currentChoices)
+        {
+            Button choiceButton = Instantiate(buttonPrefab) as Button;
+
+            choiceButton.transform.SetParent(this.transform, false);
+            Text choiceText = choiceButton.GetComponentInChildren<Text>();
+            choiceText.text = choice.text;
+
+            choiceButton.onClick.AddListener(delegate { 
+                chooseStoryChoice(choice);
+            });
+        }
+    }
+
+    void eraseUI() 
+    {
+        for(int i =  0; i <this.transform.childCount; i++) 
+        {
+            Destroy(this.transform.GetChild(i).gameObject);
+        }
+    }
+
+    string loadStoryChunk() 
+    {
+        string text = "";
+
+        if(story.canContinue) 
+        {
+            text = story.ContinueMaximally();
+        }
+
+        Debug.Log(text);
+        
+        return text;
+    }
+
+    void chooseStoryChoice(Choice choice)
+    {
+        story.ChooseChoiceIndex(choice.index);
+        refreshUI();
+    }
+}
